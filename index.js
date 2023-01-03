3f9bbbbba1f6cf955a4fbb39ab55ac382460b152
Eddie <eduard.raducanu09@gmail.com>
2023-01-03 23:30:00 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+//check button is pressed (if so, it will check WHAT button was pressed)
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
+}
+//checks to see what button was pressed, and if one of the cases is valid,
+//it will output the specific sound
+document.addEventListener("keypress", function(event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+          let crash = new Audio("sounds/crash.mp3");
+          crash.play();
+          break;
+  
+        case "a":
+          let kick = new Audio("sounds/kick-bass.mp3");
+          kick.play();
+          break;
+  
+        case "s":
+          let snare = new Audio("sounds/snare.mp3");
+          snare.play();
+          break;
+  
+        case "d":
+          let tom1 = new Audio("sounds/tom-1.mp3");
+          tom1.play();
+          break;
+  
+        case "j":
+          let tom2 = new Audio("sounds/tom-2.mp3");
+          tom2.play();
+          break;
+  
+        case "k":
+          let tom3 = new Audio("sounds/tom-3.mp3");
+          tom3.play();
+          break;
+  
+        case "l":
+          let tom4 = new Audio("sounds/tom-4.mp3");
+          tom4.play();
+          break;
+  
+        default:
+          console.log(buttonInnerHTML);
+          break;
+      }
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100)
+}
